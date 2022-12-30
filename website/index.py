b195a5e7ca9e721dab4012bf75a3d53f97f5115d
schleising <stephen@schleising.net>
2022-12-30 10:48:34 +0000

    Use templating properly

--- website/index.py
+++ website/index.py
@@ -9,4 +9,4 @@ app = FastAPI()
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     print('Hello')
-    return TEMPLATES.TemplateResponse('base.html', {'request': request})
+    return TEMPLATES.TemplateResponse('index.html', {'request': request})
