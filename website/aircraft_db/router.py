f66279d80b54513d89d7e8c290b0fe2b55a756a9
schleising <stephen@schleising.net>
2022-12-31 18:39:22 +0000

    Change the tail number to upper case

--- website/aircraft_db/router.py
+++ website/aircraft_db/router.py
@@ -19,5 +19,5 @@ async def get_ac(tail_no: str | None = None):
 
     if tail_no is not None:
         # Get the aircraft with the registration field equal to the requested tail number
-        ac = await reader.get_ac_by_tail_no(tail_no)
+        ac = await reader.get_ac_by_tail_no(tail_no.upper())
         return ac
