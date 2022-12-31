89142834745392a12ba235bb644cb90aa0a96b15
schleising <stephen@schleising.net>
2022-12-31 15:54:56 +0000

    Create a page for looking up aircraft details

--- website/aircraft_db/router.py
+++ website/aircraft_db/router.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from .aircraft_model import AircraftModel
 from .aircraft_reader import IrcaReader
 
+TEMPLATES = Jinja2Templates('/app/templates')
+
 aircraft_router = APIRouter(prefix='/aircraft')
 
-@aircraft_router.get('/', response_model=AircraftModel | None)
+@aircraft_router.get('/', response_class=HTMLResponse)
+async def get_aircraft_page(request: Request):
+    return TEMPLATES.TemplateResponse('aircraft_db/aircraft_template.html', {'request': request})
+
+@aircraft_router.get('/details', response_model=AircraftModel | None)
 async def get_ac(tail_no: str | None = None):
     reader = IrcaReader()
 
