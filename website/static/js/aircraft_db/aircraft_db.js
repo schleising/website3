9b4585772f5d96f441009015d5a640be530696cd
schleising <stephen@schleising.net>
2023-01-01 15:29:52 +0000

    Make data request generic and move to base.js

--- website/static/js/aircraft_db/aircraft_db.js
+++ website/static/js/aircraft_db/aircraft_db.js
@@ -1,23 +1,4 @@
-function checkForEnter(event) {
-    if (event.key == "Enter") {
-        searchClicked()
-    }
-}
-
-function searchClicked() {
-    var xmlhttp = new XMLHttpRequest();
-    var url = "/aircraft/tail_no/" + document.getElementById("input_tail_number").value
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var jsn = this.responseText;
-            newData(jsn);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-};
-
-function newData(jsn) {
+function parseAircraftData(jsn) {
     const dataset = JSON.parse(jsn);
     for (var key in dataset) {
         var element = document.getElementById(key)
--- website/static/js/base.js
+++ website/static/js/base.js
@@ -5,3 +5,22 @@ updateSize()
 function updateSize() {
     document.getElementById("outer").style.height = window.innerHeight + "px";
 };
+
+function checkForEnter(event, baseUrl, element, callback) {
+    if (event.key == "Enter") {
+        searchClicked(baseUrl, element, callback)
+    }
+}
+
+function searchClicked(baseUrl, element, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    var url = baseUrl + document.getElementById(element).value
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var jsn = this.responseText;
+            callback(jsn);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+};
