035d0c7f79de78b9cf595963470bb2aa6fcdb3a4
schleising <stephen@schleising.net>
2023-01-01 14:40:22 +0000

    Remove display of height and width

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -3,7 +3,5 @@ window.onresize = updateSize;
 updateSize()
 
 function updateSize() {
-    document.getElementById("width_div").innerHTML = "Width : " + window.innerWidth;
-    document.getElementById("height_div").innerHTML = "Height: " + window.innerHeight;
     document.getElementById("outer").style.height = window.innerHeight + "px";
 };
