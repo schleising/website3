6d38a4a0517506f6ddf7b789f3a1136461224b11
schleising <stephen@schleising.net>
2023-01-01 15:58:23 +0000

    Add a hamburger button to reveal the sidebar if hidden

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -1,16 +1,34 @@
 window.onresize = updateSize;
 
-updateSize()
+updateSize();
 
 function updateSize() {
     document.getElementById("outer").style.height = window.innerHeight + "px";
+
+    if (window.innerWidth > 500) {
+        document.getElementById("sidebar").style.display = "flex";
+        document.getElementById("hamburger").style.display = "none";
+    } else {
+        document.getElementById("sidebar").style.display = "none";
+        document.getElementById("hamburger").style.display = "block";
+    }
+};
+
+function hamburgerClicked() {
+    sidebarElement = document.getElementById("sidebar")
+
+    if (sidebarElement.style.display == "flex") {
+        sidebarElement.style.display = "none";
+    } else {
+        sidebarElement.style.display = "flex";
+    }
 };
 
 function checkForEnter(event, baseUrl, element, callback) {
     if (event.key == "Enter") {
         searchClicked(baseUrl, element, callback)
     }
-}
+};
 
 function searchClicked(baseUrl, element, callback) {
     var xmlhttp = new XMLHttpRequest();
