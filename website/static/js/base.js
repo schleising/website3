e274bf5896f8b0ece54901038df8eeb770221897
schleising <stephen@schleising.net>
2023-01-01 13:51:56 +0000

    Format screen dimension display better

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -3,6 +3,6 @@ window.onresize = updateSize;
 updateSize()
 
 function updateSize() {
-    document.getElementById("width_div").innerHTML = window.innerWidth;
-    document.getElementById("height_div").innerHTML = window.innerHeight;
+    document.getElementById("width_div").innerHTML = "Width : " + window.innerWidth;
+    document.getElementById("height_div").innerHTML = "Height: " + window.innerHeight;
 };
