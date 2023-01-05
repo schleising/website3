5dee1b95a185b9a154cc523e82fc5f4ef071cb89
schleising <stephen@schleising.net>
2023-01-05 14:44:04 +0000

    Ensure socket is reopened if it has been closed due to, e.g., network loss

--- website/static/js/markdown/markdown.js
+++ website/static/js/markdown/markdown.js
@@ -1,15 +1,31 @@
 // Variable which will contain the websocket
 var ws
 
+// Variable which will contain the websocket url
+var url
+
 // Add a callback for state changes
 document.addEventListener('readystatechange', readyStateChanged)
 
+// Add a callback for key up
 document.getElementById("markdown-editor-textarea").addEventListener("keyup", function (event) {
-    sendMessage(event)
+    onSendMessage(event)
 });
 
 // Disable the text entry box while the page loads
 document.getElementById("markdown-editor-textarea").disabled = true;
+
+// Function to open a web socket
+function openWebSocket() {
+    // Create a new WebSocket
+    ws = new WebSocket(url);
+
+    // Setup callback for onmessage event
+    ws.onmessage = function(event) {
+        // When a message is received from the server, set it as the innerHTML value
+        document.getElementById("markdown-output").innerHTML = event.data;
+    };
+};
 
 function readyStateChanged(event) {
     // Check the page has completely loaded
@@ -18,7 +34,7 @@ function readyStateChanged(event) {
         textareaAcceptTab("markdown-editor-textarea");
 
         // Get the page URL
-        var url = document.URL;
+        url = document.URL;
 
         // Replace the http or https with ws or wss respectively
         if ( url.startsWith("https") ) {
@@ -30,29 +46,38 @@ function readyStateChanged(event) {
         // Append the ws to the URL
         url = url + "ws";
         
-        console.log(url);
-
-        // Create a new WebSocket
-        ws = new WebSocket(url);
-
-        // Setup callback for onmessage event
-        ws.onmessage = function(event) {
-            // When a message is received from the server, set it as the innerHTML value
-            document.getElementById("markdown-output").innerHTML = event.data;
-        };
+        // Create the new socket
+        openWebSocket();
 
         // Enable the text input now that everything is ready
         document.getElementById("markdown-editor-textarea").disabled = false;
     }
 };
 
-function sendMessage(event) {
+function onSendMessage(event) {
+    // Not sure why we need this...
+    event.preventDefault();
+
+    // If the socket is not open, open a new one and wait for it to be ready
+    if (ws.readyState != WebSocket.OPEN) {
+        // Open the new socket
+        openWebSocket();
+
+        // Add the event listener
+        ws.addEventListener('open', (event) => {
+            // Once the new socket is open send the message
+            sendMessage();
+        })
+    } else {
+        // If the socket is already open, just send the message
+        sendMessage();
+    }
+};
+
+function sendMessage() {
     // Get the text from the text area and create a JSON object from it
-    var body = {text: document.getElementById("markdown-editor-textarea").value}
+    var body = {text: document.getElementById("markdown-editor-textarea").value};
 
     // Convert the JSON to a string and send it to the server
-    ws.send(JSON.stringify(body))
-
-    // Not sure why we need this...
-    event.preventDefault()
+    ws.send(JSON.stringify(body));
 };
