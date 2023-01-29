24a79e90b7e647c41db15215744e99a215d227d7
schleising <stephen@schleising.net>
2023-01-29 10:55:19 +0000

    Fix return type of get_ac_by_tail_no

--- website/aircraft_db/aircraft_reader.py
+++ website/aircraft_db/aircraft_reader.py
@@ -1,3 +1,5 @@
+from typing import Any, Mapping
+
 from . import irca_collection
 from .aircraft_model import AircraftModel
 
@@ -8,7 +10,11 @@ class IrcaReader:
     async def get_ac_by_tail_no(self, tail_no: str) -> AircraftModel | None:
         if irca_collection is not None:
             # Get the aircraft with the registration field equal to the requested tail number
-            ac: AircraftModel | None = await irca_collection.find_one({'registration': tail_no})
-            return ac
+            ac: Mapping[str, Any] | None = await irca_collection.find_one({'registration': tail_no})
+
+            if ac is not None:
+                return AircraftModel(**ac)
+            else:
+                return None
         else:
             return None
