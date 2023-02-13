a8af3125dbf11040ad773bebaac9fed2918210e3
schleising <stephen@schleising.net>
2023-02-13 11:44:15 +0000

    Exclude localhost from analytics

--- website/static/js/analytics.js
+++ website/static/js/analytics.js
@@ -1,5 +1,7 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+if (location.hostname != 'localhost') {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-gtag('config', 'G-SN5Z0MM3D8');
+    gtag('config', 'G-SN5Z0MM3D8');
+}
