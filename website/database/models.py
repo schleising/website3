3e8feb1b29a1d6ac3658d5668269aee243c1d95c
schleising <stephen@schleising.net>
2023-08-27 15:36:32 +0100

    Fix PyObjectId

--- website/database/models.py
+++ website/database/models.py
@@ -1,16 +1,17 @@
+from pydantic import FieldValidationInfo
 from bson.objectid import ObjectId
 
 class PyObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
-
+    
     @classmethod
-    def validate(cls, v):
+    def validate(cls, v: str, _: FieldValidationInfo):
         if not ObjectId.is_valid(v):
             raise ValueError("Invalid objectid")
         return ObjectId(v)
 
     @classmethod
-    def __modify_schema__(cls, field_schema):
+    def __get_pydantic_json_schema__(cls, field_schema):
         field_schema.update(type="string")
