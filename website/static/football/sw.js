b58fba6ffc08796bb2a5b72e6187905c25d75911
schleising <stephen@schleising.net>
2025-03-21 20:55:24 +0000

    Add service worker files for push notifications and fetch events

---
+++ website/static/football/sw.js
@@ -0,0 +1,48 @@
+self.addEventListener('push', function (event) {
+    console.log('Push received:', event);
+
+    var data = {};
+    if (event.data) {
+        data = event.data.json();
+    }
+
+    var title = data.title || 'Push Notification';
+    var options = {
+        body: data.body || 'This is a push notification.',
+        icon: data.icon,
+        badge: data.badge,
+        data: {
+            url: data.url || '/'
+        },
+        requireInteraction: data.requireInteraction || false
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(title, options)
+    );
+});
+
+self.onnotificationclick = (event) => {
+    console.log("On notification click: ", event.notification.tag);
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(
+        clients
+            .matchAll({
+                type: "window",
+            })
+            .then((clientList) => {
+                for (const client of clientList) {
+                    if (client.url === event.notification.data.url && "focus" in client) return client.focus();
+                }
+                if (clients.openWindow) return clients.openWindow(event.notification.data.url);
+            }),
+    );
+};
+
+// Add a fetch event listener to the service worker
+self.addEventListener('fetch', function (event) {
+    console.log('Fetch event:', event);
+});
