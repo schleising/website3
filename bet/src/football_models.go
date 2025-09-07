b791635412cd863768b78989abd4def9bbfa51de
schleising <stephen@schleising.net>
2025-09-07 13:34:47 +0100

    Refactor football_models.go to simplify struct field tags for improved readability

--- bet/src/football_models.go
+++ bet/src/football_models.go
@@ -3,26 +3,26 @@ package main
 import "time"
 
 type Area struct {
-	Id   int    `json:"id" bson:"id"`
-	Name string `json:"name" bson:"name"`
-	Code string `json:"code" bson:"code"`
-	Flag string `json:"flag" bson:"flag"`
+	Id   int
+	Name string
+	Code string
+	Flag string
 }
 
 type Competition struct {
-	Id     int    `json:"id" bson:"id"`
-	Name   string `json:"name" bson:"name"`
-	Code   string `json:"code" bson:"code"`
-	Type   string `json:"type" bson:"type"`
-	Emblem string `json:"emblem" bson:"emblem"`
+	Id     int
+	Name   string
+	Code   string
+	Type   string
+	Emblem string
 }
 
 type Team struct {
-	Id        int    `json:"id" bson:"id"`
-	Name      string `json:"name" bson:"name"`
-	ShortName string `json:"short_name" bson:"short_name"`
-	Tla       string `json:"tla" bson:"tla"`
-	Crest     string `json:"crest" bson:"crest"`
+	Id        int
+	Name      string
+	ShortName string `bson:"short_name"`
+	Tla       string
+	Crest     string
 }
 
 func (t *Team) GetShortName() string {
@@ -39,11 +39,11 @@ func (t *Team) GetShortName() string {
 }
 
 type Season struct {
-	Id              int    `json:"id" bson:"id"`
-	StartDate       string `json:"start_date" bson:"start_date"`
-	EndDate         string `json:"end_date" bson:"end_date"`
-	CurrentMatchday int    `json:"current_matchday" bson:"current_matchday"`
-	Winner          Team   `json:"winner" bson:"winner"`
+	Id              int
+	StartDate       string `bson:"start_date"`
+	EndDate         string `bson:"end_date"`
+	CurrentMatchday int    `bson:"current_matchday"`
+	Winner          Team
 }
 
 type MatchStatus string
@@ -61,52 +61,52 @@ const (
 )
 
 type FullTime struct {
-	Home int `json:"home" bson:"home"`
-	Away int `json:"away" bson:"away"`
+	Home int
+	Away int
 }
 
 type HalfTime struct {
-	Home int `json:"home" bson:"home"`
-	Away int `json:"away" bson:"away"`
+	Home int
+	Away int
 }
 
 type Score struct {
-	Winner   string   `json:"winner" bson:"winner"`
-	Duration string   `json:"duration" bson:"duration"`
-	FullTime FullTime `json:"full_time" bson:"full_time"`
-	HalfTime HalfTime `json:"half_time" bson:"half_time"`
+	Winner   string
+	Duration string
+	FullTime FullTime `bson:"full_time"`
+	HalfTime HalfTime `bson:"half_time"`
 }
 
 type Odds struct {
-	Msg string `json:"msg" bson:"msg"`
+	Msg string `bson:"msg"`
 }
 
 type Referee struct {
-	Id          int    `json:"id" bson:"id"`
-	Name        string `json:"name" bson:"name"`
-	Type        string `json:"type" bson:"type"`
-	Nationality string `json:"nationality" bson:"nationality"`
+	Id          int
+	Name        string
+	Type        string
+	Nationality string
 }
 
 type Match struct {
-	Area        Area        `json:"area" bson:"area"`
-	Competition Competition `json:"competition" bson:"competition"`
-	Season      Season      `json:"season" bson:"season"`
-	Id          int         `json:"id" bson:"id"`
-	UtcDate     time.Time   `json:"utc_date" bson:"utc_date"`
-	LocalDate   time.Time   `json:"local_date" bson:"local_date"`
-	Status      MatchStatus `json:"status" bson:"status"`
-	Minute      int         `json:"minute" bson:"minute"`
-	InjuryTime  int         `json:"injury_time" bson:"injury_time"`
-	Matchday    int         `json:"matchday" bson:"matchday"`
-	Stage       string      `json:"stage" bson:"stage"`
-	Group       string      `json:"group" bson:"group"`
-	LastUpdated time.Time   `json:"last_updated" bson:"last_updated"`
-	HomeTeam    Team        `json:"home_team" bson:"home_team"`
-	AwayTeam    Team        `json:"away_team" bson:"away_team"`
-	Score       Score       `json:"score" bson:"score"`
-	Odds        Odds        `json:"odds" bson:"odds"`
-	Referees    []Referee   `json:"referees" bson:"referees"`
+	Area        Area
+	Competition Competition
+	Season      Season
+	Id          int
+	UtcDate     time.Time `bson:"utc_date"`
+	LocalDate   time.Time `bson:"local_date"`
+	Status      MatchStatus
+	Minute      int
+	InjuryTime  int `bson:"injury_time"`
+	Matchday    int
+	Stage       string
+	Group       string
+	LastUpdated time.Time `bson:"last_updated"`
+	HomeTeam    Team      `bson:"home_team"`
+	AwayTeam    Team      `bson:"away_team"`
+	Score       Score
+	Odds        Odds
+	Referees    []Referee
 }
 
 func (match *Match) HasStarted() bool {
@@ -162,26 +162,26 @@ func (match *Match) TeamPoints(teamName string) int {
 }
 
 type FormItem struct {
-	Character string `json:"character" bson:"character"`
-	CssClass  string `json:"css_class" bson:"css_class"`
+	Character string
+	CssClass  string `bson:"css_class"`
 }
 
 type LiveTableItem struct {
-	Position       int        `json:"position" bson:"position"`
-	Team           Team       `json:"team" bson:"team"`
-	PlayedGames    int        `json:"played_games" bson:"played_games"`
-	Form           string     `json:"form" bson:"form"`
-	Won            int        `json:"won" bson:"won"`
-	Draw           int        `json:"draw" bson:"draw"`
-	Lost           int        `json:"lost" bson:"lost"`
-	Points         int        `json:"points" bson:"points"`
-	GoalsFor       int        `json:"goals_for" bson:"goals_for"`
-	GoalsAgainst   int        `json:"goals_against" bson:"goals_against"`
-	GoalDifference int        `json:"goal_difference" bson:"goal_difference"`
-	HasStarted     bool       `json:"has_started" bson:"has_started"`
-	IsHalftime     bool       `json:"is_halftime" bson:"is_halftime"`
-	HasFinished    bool       `json:"has_finished" bson:"has_finished"`
-	ScoreString    *string    `json:"score_string" bson:"score_string"`
-	CssClass       *string    `json:"css_class" bson:"css_class"`
-	FormList       []FormItem `json:"form_list" bson:"form_list"`
+	Position       int
+	Team           Team
+	PlayedGames    int `bson:"played_games"`
+	Form           string
+	Won            int
+	Draw           int
+	Lost           int
+	Points         int
+	GoalsFor       int        `bson:"goals_for"`
+	GoalsAgainst   int        `bson:"goals_against"`
+	GoalDifference int        `bson:"goal_difference"`
+	HasStarted     bool       `bson:"has_started"`
+	IsHalftime     bool       `bson:"is_halftime"`
+	HasFinished    bool       `bson:"has_finished"`
+	ScoreString    *string    `bson:"score_string"`
+	CssClass       *string    `bson:"css_class"`
+	FormList       []FormItem `bson:"form_list"`
 }
