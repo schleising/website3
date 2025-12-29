b8838f7fdd4fb225d057538df535102d2a6df0fb
schleising <stephen@schleising.net>
2025-12-29 11:58:13 +0000

    Remove Google Analytics script for improved privacy compliance

--- website/static/js/analytics.js
+++
@@ -1,7 +0,0 @@
-if (location.hostname != 'localhost') {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-SN5Z0MM3D8');
-}
